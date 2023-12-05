6fba8389d3b317d8bc59eb4a9fa410bf8170ee6e
Björn Mårtensson <bjorn.martensson@irfu.se>
2023-12-05 14:10:48 +0100

    Dag 5

--- day5/main.py
+++ day5/main.py
@@ -12,8 +12,8 @@ def part1(filename):
 	with io.open(filename, mode = 'r') as file:
 		input = file.read()
 
-	regex = re.compile(r"seeds: (?P<seeds>(\d+ ?)+)|(?P<header_from>\w+)-to-(?P<header_to>\w+) map:|(?P<to>\d+) (?P<from>\d+) (?P<length>\d+)")
-	almanac = dict()
+	regex = re.compile(r"seeds: (?P<seeds>(\d+ ?)+)|(?P<header_from>\w+)-to-(?P<header_to>\w+) map:|(?P<to>\d+) (?P<from>\d+) (?P<length>\d+)|(?P<end_context>\n\n)")
+	almanac = collections.defaultdict(list)
 	from_context = None
 	to_context = "seed"
 
@@ -21,15 +21,23 @@ def part1(filename):
 		if seeds := input_match["seeds"]:
 			almanac["seed"] = [int(seed) for seed in seeds.strip().split()]
 		elif input_match["header_from"]:
+			almanac[to_context] += almanac[from_context]
 			from_context = input_match["header_from"]
 			to_context = input_match["header_to"]
 			pass
 		elif (from_start := input_match["from"]) and (to_start := input_match["to"]) and (range_length := input_match["length"]):
 			from_range = range(int(from_start), int(from_start) + int(range_length))
 			to_offset = int(to_start) - int(from_start)
-			for item in almanac[from_context]:
+			from_items = almanac[from_context]
+			i = 0
+			while i < len(from_items):
+				item = from_items[i]
 				if item in from_range:
-					almanac.setdefault(to_context, []).append(item + to_offset)
+					almanac[to_context].append(item + to_offset)
+					from_items[i:i+1] = []
+				else:
+					i += 1
+	almanac[to_context] += almanac[from_context]
 
 	print("Part 1: {}".format(min(almanac["location"])))
 
@@ -37,7 +45,7 @@ def part2(filename):
 	with io.open(filename, mode = 'r') as file:
 		input = file.read()
 
-	regex = re.compile(r"seeds: (?P<seeds>(\d+ ?)+)|(?P<header_from>\w+)-to-(?P<header_to>\w+) map:|(?P<to>\d+) (?P<from>\d+) (?P<length>\d+)")
+	regex = re.compile(r"seeds: (?P<seeds>(\d+ ?)+)|(?P<header_from>\w+)-to-(?P<header_to>\w+) map:|(?P<to>\d+) (?P<from>\d+) (?P<length>\d+)|(?P<end_context>\n\n)")
 	almanac = collections.defaultdict(list)
 	from_context = None
 	to_context = "seed"
@@ -46,15 +54,27 @@ def part2(filename):
 		if seeds := input_match["seeds"]:
 			almanac["seed"] = [range(int(seed_start), int(seed_start) + int(seed_length)) for seed_start, seed_length in re.findall(r"(\d+) (\d+)", seeds)]
 		elif input_match["header_from"]:
+			almanac[to_context] += almanac[from_context]
 			from_context = input_match["header_from"]
 			to_context = input_match["header_to"]
 			pass
 		elif (from_start := input_match["from"]) and (to_start := input_match["to"]) and (range_length := input_match["length"]):
 			from_range = range(int(from_start), int(from_start) + int(range_length))
 			to_offset = int(to_start) - int(from_start)
-			for item in almanac[from_context]:
+			from_items = almanac[from_context]
+			i = 0
+			while i < len(from_items):
+				item = from_items[i]
 				if intersection := range(max(item.start, from_range.start), min(item.stop, from_range.stop)):
+					from_items[i:i+1] = []
+					if disjunction_before := range(item.start, intersection.start):
+						almanac[from_context].append(disjunction_before)
+					if disjunction_after := range(intersection.stop, item.stop):
+						almanac[from_context].append(disjunction_after)
 					almanac[to_context].append(range(intersection.start + to_offset, intersection.stop + to_offset))
+				else:
+					i += 1
+	almanac[to_context] += almanac[from_context]
 
 	print("Part 2: {}".format(min(location.start for location in almanac["location"])))
 
