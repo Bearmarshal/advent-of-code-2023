59faa2776409924d95ef85b25d38e69213a64b10
3jörn Mårtensson <bearmarshal@gmail.com>
2023-12-20 21:01:28 +0100

    Dag 20: Mestadels analytisk lösning, inte generell

--- day20/main.py
+++ day20/main.py
@@ -12,20 +12,10 @@ class Module:
 		self.module_type = module_type
 		self.name = name
 		self.destinations = destinations
-		match self.module_type:
-			case "%":
-				self.state = False
-			case "&":
-				self.state = dict()
-			case "":
-				self.state = None
-
-	def copy(self):
-		copy = Module(self.module_type, self.name, self.destinations)
-		copy.state = self.state
-		return copy
+		self.sources = dict()
+		self.state = False
 	
-	def pulse(self, pulse):
+	def pulse(self, source, pulse):
 		match self.module_type:
 			case "%":
 				if not pulse:
@@ -34,7 +24,8 @@ class Module:
 				else:
 					return None
 			case "&":
-				return not all(self.state.values())
+				self.sources[source] = pulse
+				return not all(self.sources.values())
 			case "":
 				return pulse
 
@@ -46,35 +37,70 @@ def part1(filename):
 		modules[name] = Module(module_type, name, destinations.split(", "))
 	for name in modules:
 		for destination in modules[name].destinations:
-			if destination in modules and modules[destination].module_type == "&":
-				modules[destination].state[name] = False
+			if destination in modules:
+				modules[destination].sources[name] = False
 	num_pulses = {True: 0, False: 0}
+	pulses = collections.deque()
 	for _ in range(1000):
-		num_pulses[False] += 1
-		next_state = {name: module.copy() for name, module in modules.items()}
-		pulse_map = {"broadcaster": False}
-		while pulse_map:
-			next_pulse_map = dict()
-			for name, pulse in pulse_map.items():
+		pulses.append(("button", "broadcaster", False))
+		while pulses:
+			source, name, pulse = pulses.popleft()
+			num_pulses[pulse] += 1
+			if name in modules:
 				module = modules[name]
-				output = module.pulse(pulse)
-				if module.module_type == "%":
-					next_state[name].state = module.state
+				output = module.pulse(source, pulse)
 				if output != None:
 					for destination in module.destinations:
-						if destination in modules:
-							next_pulse_map[destination] = output
-							if modules[destination].module_type == "&":
-								next_state[destination].state[name] = output
-						num_pulses[output] += 1
-			pulse_map = next_pulse_map
-	print(f"low: {num_pulses[False]}, high: {num_pulses[True]}")
+						pulses.append((name, destination, output))
 	print("Part 1: {}".format(num_pulses[True] * num_pulses[False]))
 
 def part2(filename):
 	with io.open(filename, mode = 'r') as file:
-		input = [line.strip() for line in file if not line.isspace()]
-	print("Part 2: {}".format(2))
+		input = file.read().strip()
+	modules = dict()
+	for module_type, name, destinations in re.findall(r"(?P<type>[%&]?)(?P<name>\w+) -> (\w+(?:, \w+)*)", input):
+		modules[name] = Module(module_type, name, destinations.split(", "))
+	for name in modules:
+		for destination in modules[name].destinations:
+			if destination in modules:
+				modules[destination].sources[name] = False
+	traces = collections.deque()
+	traces.append(([], "broadcaster"))
+	loops = dict()
+	while traces:
+		visited, name = traces.popleft()
+		if name in visited:
+			loop = visited[visited.index(name):]
+			lowest_index = loop.index(min(loop))
+			loops["->".join(loop[lowest_index:] + loop[:lowest_index] + loop[lowest_index:lowest_index + 1])] = loop[lowest_index:] + loop[:lowest_index]
+		elif name in modules:
+			module = modules[name]
+			for destination in module.destinations:
+				traces.append((visited + [name], destination))
+	# print(len(loops))
+	# for loop, contained_modules in loops.items():
+	# 	print(sum(modules[module].module_type == "&" for module in contained_modules))
+	pulses = collections.deque()
+	cycle_length = {module: 0 for module in ["xd", "gt", "zt", "ms"]}
+	num_pulses = 0
+	while not all (cycle_length.values()):
+		num_pulses += 1
+		pulses.append(("button", "broadcaster", False))
+		while pulses:
+			source, name, pulse = pulses.popleft()
+			if name in modules:
+				module = modules[name]
+				output = module.pulse(source, pulse)
+				if output != None:
+					for destination in module.destinations:
+						pulses.append((name, destination, output))
+				if name in cycle_length and output == False and not cycle_length[name]:
+					cycle_length[name] = num_pulses
+	accumulator = 1
+	for cycle in cycle_length.values():
+		divisor = math.gcd(accumulator, cycle)
+		accumulator *= cycle // divisor
+	print("Part 2: {}".format(accumulator))
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1:
@@ -82,4 +108,4 @@ if __name__ == "__main__":
 	else:
 		filename = os.path.dirname(sys.argv[0]) + "/input.txt"
 	part1(filename)
-	# part2(filename)
+	part2(filename)
