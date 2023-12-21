4b68bae327b5ef2f27e4d213447ff66c24de0edc
Björn Mårtensson <bjorn.martensson@irfu.se>
2023-12-21 18:01:45 +0100

    Dag 21: Del 2 funkar inte än

--- day21/main.py
+++ day21/main.py
@@ -83,7 +83,68 @@ def part1(filename):
 	print("Part 1: {}".format(sum(1 - steps % 2 for steps in closed_set.values())))
 
 def part2(filename):
-	print("Part 2: {}".format(2))
+	with io.open(filename, mode = 'r') as file:
+		field_map = [line.strip() for line in file if not line.isspace()]
+	height = len(field_map)
+	width = len(field_map[0])
+	print(f"height = {height}, width = {width}")
+	for y, row in enumerate(field_map):
+		if "S" in row:
+			start = Position(y, row.index("S"))
+	## The row and column of the starting point, and the outer edges are all empty, so the closest entry point in each repeated
+	## field is the closest corner to or on the same row/column as the starting point, and the distance is just the Manhattan distance
+	entry_points = dict()
+	for y, dy in ((0, height - start.y), (start.y, 0), (height - 1, start.y + 1)):
+		for x, dx in ((0, width - start.x), (start.x, 0), (width - 1, start.x + 1)):
+			entry_points[Position(y, x)] = dy + dx
+	# steps_remaining = 26501365
+	steps_remaining = 100
+	congruence = steps_remaining % 2
+	num_reachable = 0
+	for point, distance_from_start in entry_points.items():
+		closed_set = {point: 0}
+		open_set = collections.deque([(point, 0)])
+		while open_set:
+			position, steps = open_set.popleft()
+			steps += 1
+			for direction in Direction:
+				neighbour = position + direction
+				if neighbour not in closed_set and neighbour.get_item(field_map) == "." or neighbour.get_item(field_map) == "S":
+					open_set.append((neighbour, steps))
+					closed_set[neighbour] = steps
+		distances_from_entry_point = {position: steps for position, steps in closed_set.items() if (steps + distance_from_start) % 2 == 0}, {position: steps for position, steps in closed_set.items() if (steps + distance_from_start) % 2 == 1}
+		if point == start:
+			num_reachable += len(distances_from_entry_point[congruence])
+			print()
+			print(f"reachable in one field = {len(distances_from_entry_point[congruence])}")
+		else:
+			## Abusing that width == height and that all entry points are congruent with start in neigbouring fields
+			num_entered_fields = 1 + (steps_remaining - distance_from_start) // width
+			max_distance_in_field = max(max(distances_from_entry_point[0].values()), max(distances_from_entry_point[1].values()))
+			num_full_fields = 1 + (steps_remaining - distance_from_start - max_distance_in_field) // width
+			print()
+			print(f"distance from start = {distance_from_start}, entered = {num_entered_fields}, fully = {num_full_fields}")
+			# print(f"max distance from entry point = {max(distances_from_entry_point.values())}")
+			if point.y != start.y and point.x != start.x:
+				## Diagonals
+				print(f"entered = {num_entered_fields * (num_entered_fields - 1) // 2}, fully = {num_full_fields * (num_full_fields - 1) // 2}")
+				num_full_congruent = ((num_full_fields + 1) // 2) ** 2
+				num_full_not_congruent = num_full_fields * (num_full_fields - 1) // 2 - num_full_congruent
+				num_reachable += num_full_congruent * len(distances_from_entry_point[congruence]) + num_full_not_congruent * len(distances_from_entry_point[1 - congruence])
+				for fields in range(num_full_fields, num_entered_fields):
+					steps_remaining_at_field = steps_remaining - distance_from_start - fields * width
+					congruence_index = congruence if fields % 2 == 0 else 1 - congruence
+					num_reachable += (fields + 1) * len([steps for steps in distances_from_entry_point[congruence_index].values() if steps <= steps_remaining_at_field])
+			else:
+				print(f"entered = {num_entered_fields}, fully = {num_full_fields}")
+				num_full_congruent = (num_full_fields + 1) // 2
+				num_full_not_congruent = num_full_fields // 2
+				num_reachable += num_full_congruent * len(distances_from_entry_point[congruence]) + num_full_not_congruent * len(distances_from_entry_point[1 - congruence])
+				for fields in range(num_full_fields, num_entered_fields):
+					steps_remaining_at_field = steps_remaining - distance_from_start - fields * width
+					congruence_index = congruence if fields % 2 == 0 else 1 - congruence
+					num_reachable += len([steps for steps in distances_from_entry_point[congruence_index].values() if steps <= steps_remaining_at_field])
+	print("Part 2: {}".format(num_reachable))
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1:
@@ -91,4 +152,4 @@ if __name__ == "__main__":
 	else:
 		filename = os.path.dirname(sys.argv[0]) + "/input.txt"
 	part1(filename)
-	# part2(filename)
+	part2(filename)
