4c6569a9eb711364798d85dbcd2b1d6f7bb71a21
3jörn Mårtensson <bearmarshal@gmail.com>
2023-12-22 00:14:25 +0100

    Dag 21

--- day21/main.py
+++ day21/main.py
@@ -87,18 +87,36 @@ def part2(filename):
 		field_map = [line.strip() for line in file if not line.isspace()]
 	height = len(field_map)
 	width = len(field_map[0])
-	print(f"height = {height}, width = {width}")
 	for y, row in enumerate(field_map):
 		if "S" in row:
 			start = Position(y, row.index("S"))
-	## The row and column of the starting point, and the outer edges are all empty, so the closest entry point in each repeated
-	## field is the closest corner to or on the same row/column as the starting point, and the distance is just the Manhattan distance
+
+	## This solver makes the following assumptions:
+	## - The field is a square with odd-valued dimensions
+	## - The row and column of the starting point and the outer edges are all empty, so the closest entry point in each repeated
+	##   field is on the same row/column as the starting point (cardinal directions), or the closest corner (diagonal directions).
+	##
+	## From these assumptions we can deduce:
+	## - The shortest distance from the starting point to each entry point in every field is just the Manhattan distance
+	## - Each field in the same quadrant and at the same distance from the starting point has the same number of reachable points
+	##
+	## Test these assumptions:
+	if height != width or height % 2 == 0:
+		print("Invalid assumption! Expected the field to be a square with odd-valued dimensions, found {}x{}".format(height, width))
+		return
+	if "#" in field_map[0] + field_map[-1] or "#" in "".join([row[0] + row[-1] for row in field_map]):
+		print("Invalid assumption! Expected outer edges of the field to be empty")
+		return
+	if "#" in field_map[start.y] or "#" in [row[start.x] for row in field_map]:
+		print("Invalid assumption! Expected row and column of the starting point to be empty")
+		return
+
+	## Now we can solve the problem
 	entry_points = dict()
 	for y, dy in ((0, height - start.y), (start.y, 0), (height - 1, start.y + 1)):
 		for x, dx in ((0, width - start.x), (start.x, 0), (width - 1, start.x + 1)):
 			entry_points[Position(y, x)] = dy + dx
-	# steps_remaining = 26501365
-	steps_remaining = 100
+	steps_remaining = 26501365
 	congruence = steps_remaining % 2
 	num_reachable = 0
 	for point, distance_from_start in entry_points.items():
@@ -109,40 +127,34 @@ def part2(filename):
 			steps += 1
 			for direction in Direction:
 				neighbour = position + direction
-				if neighbour not in closed_set and neighbour.get_item(field_map) == "." or neighbour.get_item(field_map) == "S":
+				if neighbour not in closed_set and (neighbour.get_item(field_map) == "." or neighbour.get_item(field_map) == "S"):
 					open_set.append((neighbour, steps))
 					closed_set[neighbour] = steps
 		distances_from_entry_point = {position: steps for position, steps in closed_set.items() if (steps + distance_from_start) % 2 == 0}, {position: steps for position, steps in closed_set.items() if (steps + distance_from_start) % 2 == 1}
 		if point == start:
 			num_reachable += len(distances_from_entry_point[congruence])
-			print()
-			print(f"reachable in one field = {len(distances_from_entry_point[congruence])}")
 		else:
 			## Abusing that width == height and that all entry points are congruent with start in neigbouring fields
 			num_entered_fields = 1 + (steps_remaining - distance_from_start) // width
 			max_distance_in_field = max(max(distances_from_entry_point[0].values()), max(distances_from_entry_point[1].values()))
 			num_full_fields = 1 + (steps_remaining - distance_from_start - max_distance_in_field) // width
-			print()
-			print(f"distance from start = {distance_from_start}, entered = {num_entered_fields}, fully = {num_full_fields}")
-			# print(f"max distance from entry point = {max(distances_from_entry_point.values())}")
 			if point.y != start.y and point.x != start.x:
 				## Diagonals
-				print(f"entered = {num_entered_fields * (num_entered_fields - 1) // 2}, fully = {num_full_fields * (num_full_fields - 1) // 2}")
 				num_full_congruent = ((num_full_fields + 1) // 2) ** 2
-				num_full_not_congruent = num_full_fields * (num_full_fields - 1) // 2 - num_full_congruent
+				num_full_not_congruent = num_full_fields * (num_full_fields + 1) // 2 - num_full_congruent
 				num_reachable += num_full_congruent * len(distances_from_entry_point[congruence]) + num_full_not_congruent * len(distances_from_entry_point[1 - congruence])
-				for fields in range(num_full_fields, num_entered_fields):
-					steps_remaining_at_field = steps_remaining - distance_from_start - fields * width
-					congruence_index = congruence if fields % 2 == 0 else 1 - congruence
-					num_reachable += (fields + 1) * len([steps for steps in distances_from_entry_point[congruence_index].values() if steps <= steps_remaining_at_field])
+				for field in range(num_full_fields, num_entered_fields):
+					steps_remaining_at_field = steps_remaining - distance_from_start - field * width
+					congruence_index = congruence if field % 2 == 0 else 1 - congruence
+					num_reachable += (field + 1) * len([steps for steps in distances_from_entry_point[congruence_index].values() if steps <= steps_remaining_at_field])
 			else:
-				print(f"entered = {num_entered_fields}, fully = {num_full_fields}")
+				## Cardinals
 				num_full_congruent = (num_full_fields + 1) // 2
 				num_full_not_congruent = num_full_fields // 2
 				num_reachable += num_full_congruent * len(distances_from_entry_point[congruence]) + num_full_not_congruent * len(distances_from_entry_point[1 - congruence])
-				for fields in range(num_full_fields, num_entered_fields):
-					steps_remaining_at_field = steps_remaining - distance_from_start - fields * width
-					congruence_index = congruence if fields % 2 == 0 else 1 - congruence
+				for field in range(num_full_fields, num_entered_fields):
+					steps_remaining_at_field = steps_remaining - distance_from_start - field * width
+					congruence_index = congruence if field % 2 == 0 else 1 - congruence
 					num_reachable += len([steps for steps in distances_from_entry_point[congruence_index].values() if steps <= steps_remaining_at_field])
 	print("Part 2: {}".format(num_reachable))
 
